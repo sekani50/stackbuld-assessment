1622f23f4aa263ae6997c5ea86d9630cb2e6f02a
sekani50 <mohammedadeoye0@gmail.com>
2024-09-01 12:23:44 +0100

    efrtbfv

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,12 +3,13 @@ import Landing from "@/components/landing/Landing";
 import { getRequest } from "@/utils/server";
 import { TResponse } from "@/types";
 
-export default async function Page() {
-  const response =  await getRequest<TResponse>({endpoint:"/request"})
-console.log(response)
+export default  function Page() {
+//   const response =  await getRequest<TResponse>({endpoint:"/request"})
+// console.log(response)
+const response: any = {}
   return (
     <div className=" w-full h-full">
-      <Landing data={response.data}/>
+      <Landing data={response}/>
     </div>
   )
 }
