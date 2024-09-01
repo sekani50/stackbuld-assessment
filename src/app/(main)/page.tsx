be6fe9f50e8346029aebe459cb7c53c88f2bcc45
sekani50 <mohammedadeoye0@gmail.com>
2024-09-01 12:33:50 +0100

    fix: server

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,13 +3,13 @@ import Landing from "@/components/landing/Landing";
 import { getRequest } from "@/utils/server";
 import { TResponse } from "@/types";
 
-export default  function Page() {
-//   const response =  await getRequest<TResponse>({endpoint:"/request"})
+export default async function Page() {
+    const response =  await getRequest<TResponse>({endpoint:"/request"})
 
-const response: any = {}
+
   return (
     <div className=" w-full h-full">
-      <Landing data={response}/>
+      <Landing data={response?.data}/>
     </div>
   )
 }
