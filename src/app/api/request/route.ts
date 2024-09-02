f1303f35811604b03822b1c65a53752829d19f35
sekani50 <mohammedadeoye0@gmail.com>
2024-09-02 10:31:32 +0100

    fix: json path issue

--- src/app/api/request/route.ts
+++ src/app/api/request/route.ts
@@ -1,65 +1,73 @@
 import path from "path";
-import {promises as fs} from "fs";
+import { promises as fs } from "fs";
 import { nanoid } from "nanoid";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-        req: NextRequest) {
+export async function GET(req: NextRequest) {
+  if (req.method === "GET") {
+    try {
+      const jsonDirectory = path.join(process.cwd(), "public"); // directory path
       
-      
-        if (req.method === "GET") {
-          try {
-    
-            const jsonDirectory = path.join(process.cwd(), 'public'); // directory path
-            const categoryFileContents = await fs.readFile(jsonDirectory + '/data/categories.json', 'utf8'); // file path
-            const productFileContents = await fs.readFile(jsonDirectory + '/data/products.json', 'utf8'); // file path
+      // Clear the require cache for the JSON files
+      delete require.cache[
+        require.resolve(jsonDirectory + "/data/categories.json")
+      ];
+      delete require.cache[
+        require.resolve(jsonDirectory + "/data/products.json")
+      ];
 
-            const productData = JSON.parse(productFileContents);
-            const categoryData = JSON.parse(categoryFileContents);
+      // Read the JSON files
+      const categoryFileContents = await fs.readFile(
+        jsonDirectory + "/data/categories.json",
+        "utf8"
+      ); // file path
+      const productFileContents = await fs.readFile(
+        jsonDirectory + "/data/products.json",
+        "utf8"
+      ); // file path
 
-          
-             const categories = categoryData.map((c: any) => {
-                return {
-                 ...c,
-                  id: nanoid(),
-                };
-            
-             })   
+      const productData = JSON.parse(productFileContents);
+      const categoryData = JSON.parse(categoryFileContents);
 
-             const products = productData.map((p: any) => {
-              return {
-                ...p,
-                
-                category: categories.find((c: any) => c.name.toLowerCase() === p.category.toLowerCase()),
-                id: nanoid()
-              }
-             })
+      const categories = categoryData.map((c: any) => {
+        return {
+          ...c,
+          id: nanoid(),
+        };
+      });
 
-            
+      const products = productData.map((p: any) => {
+        return {
+          ...p,
 
-            return NextResponse.json(
-              {
-                data: {categories, products},
-              },
-              {
-                status: 200,
-              }
-            );
-          } catch (error) {
-           
-            return NextResponse.json(
-              {
-                error: "An error occurred while making the request.",
-              },
-              {
-                status: 500,
-              }
-            );
-          }
-        } else {
-          return NextResponse.json({ error: "Method not allowed" });
+          category: categories.find(
+            (c: any) => c.name.toLowerCase() === p.category.toLowerCase()
+          ),
+          id: nanoid(),
+        };
+      });
+
+      return NextResponse.json(
+        {
+          data: { categories, products },
+        },
+        {
+          status: 200,
         }
-      }
+      );
+    } catch (error) {
+      return NextResponse.json(
+        {
+          error: "An error occurred while making the request.",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  } else {
+    return NextResponse.json({ error: "Method not allowed" });
+  }
+}
 
 export const dynamic = "force-dynamic";
-      
